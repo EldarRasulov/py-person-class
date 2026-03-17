9010f58e1b3fae1c4e6ffd4879f0bb4e854f43e1
Eldar RasuLov <morddiff@gmail.com>
2026-03-17 20:25:04 +0400

    Person-class 1.2

--- app/main.py
+++ app/main.py
@@ -1,27 +1,20 @@
 class Person:
     people = {}
-
     def __init__(self, name, age):
         self.name = name
         self.age = age
         Person.people[name] = self
 
-
 def create_person_list(people: list) -> list:
     Person.people = {}
     persons = []
-
     for data in people:
         person = Person(data["name"], data["age"])
         persons.append(person)
-
     for data in people:
         person = Person.people[data["name"]]
-
         if "wife" in data and data["wife"] is not None:
             person.wife = Person.people[data["wife"]]
-
         if "husband" in data and data["husband"] is not None:
             person.husband = Person.people[data["husband"]]
-
     return persons
